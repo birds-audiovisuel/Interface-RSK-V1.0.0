03898e996361e05eadd473c05ca40e2b5c0d20d6
Charles-Adrien Vlamynck <90679974+charlesadrien24@users.noreply.github.com>
2025-01-01 19:34:31 +0100

    Update run.py

--- run.py
+++ run.py
@@ -1,8 +1,12 @@
-from src.soccer_sim import app
 from werkzeug.middleware.dispatcher import DispatcherMiddleware
+from src.soccer_sim import app
 from flask import Flask
 
-
+# Ajout d'un préfixe pour les routes
+app_with_prefix = DispatcherMiddleware(Flask('dummy_app'), {
+    '/outils/maths': app
+})
 
 if __name__ == "__main__":
-    app.run(debug=True, host='127.0.0.1')
+    from werkzeug.serving import run_simple
+    run_simple("127.0.0.1", 8080, app_with_prefix)
