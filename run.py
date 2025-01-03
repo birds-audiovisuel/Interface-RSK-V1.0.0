edab5f6179a71c0a780da64c8336acd8ca97a4f1
Charles-Adrien Vlamynck <charles-adrien@vlamynck.fr>
2025-01-03 01:30:03 +0000

    Publication V1.2 : reset du cache + modification du run.py et soccer_sim.py

--- run.py
+++ run.py
@@ -2,12 +2,10 @@ from src.soccer_sim import app
 from werkzeug.middleware.dispatcher import DispatcherMiddleware
 from flask import Flask
 
-# Ajout du préfixe "/outils/maths"
 app_with_prefix = DispatcherMiddleware(Flask('dummy_app'), {
-   '/outils/maths': app
+    '/outils/maths/': app
 })
 
 if __name__ == "__main__":
-from werkzeug.serving import run_simple
-run_simple("127.0.0.1", 7050, app_with_prefix)
-)
+    from werkzeug.serving import run_simple
+    run_simple("127.0.0.1", 7050, app_with_prefix)
